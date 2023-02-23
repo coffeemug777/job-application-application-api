e076ba76f69f05420128b44af58dfafba4e24937
Prasetyo Denny Wibowo <indocoffee@gmail.com>
2023-02-23 14:49:21 -0500

    Add service, ApplicationService, just with add, findall and find by id

---
+++ src/main/java/com/jobapplicationapp/applicationapi/service/ApplicationService.java
@@ -0,0 +1,36 @@
+package com.jobapplicationapp.applicationapi.service;
+
+import com.jobapplicationapp.applicationapi.dto.ApplicationRequest;
+import com.jobapplicationapp.applicationapi.model.Application;
+import com.jobapplicationapp.applicationapi.repository.ApplicationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigInteger;
+import java.util.List;
+
+@Service
+public class ApplicationService {
+    @Autowired
+    private ApplicationRepository applicationRepository;
+
+    public Application addApplication(ApplicationRequest application) {
+
+        return this.applicationRepository.save(Application.builder()
+                .userId(application.getUserId())
+                .firstName(application.getFirstName())
+                .lastName(application.getLastName())
+                .email(application.getEmail())
+                .phone(application.getPhone())
+
+                .build());
+    }
+
+    public List<Application> getAll() {
+        return this.applicationRepository.findAll();
+    }
+
+    public Application find(BigInteger id) {
+        return this.applicationRepository.findById(id).get();
+    }
+}
