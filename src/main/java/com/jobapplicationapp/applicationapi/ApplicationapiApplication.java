907460a708eaaf37c4946c35333189520895b3a9
Prasetyo Denny Wibowo <indocoffee@gmail.com>
2023-02-23 14:37:03 -0500

    initial commit

---
+++ src/main/java/com/jobapplicationapp/applicationapi/ApplicationapiApplication.java
@@ -0,0 +1,13 @@
+package com.jobapplicationapp.applicationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobapplicationapp/applicationapi/ApplicationapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobapplicationapp.applicationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
