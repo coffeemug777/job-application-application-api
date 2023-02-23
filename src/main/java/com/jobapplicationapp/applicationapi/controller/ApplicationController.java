2977de5d3e11edc293ede403ae2c4b1f8f2e5afc
Prasetyo Denny Wibowo <indocoffee@gmail.com>
2023-02-23 14:49:48 -0500

    Add controller, with end points application, application/id, application/all

---
+++ src/main/java/com/jobapplicationapp/applicationapi/controller/ApplicationController.java
@@ -0,0 +1,34 @@
+package com.jobapplicationapp.applicationapi.controller;
+
+import com.jobapplicationapp.applicationapi.dto.ApplicationRequest;
+import com.jobapplicationapp.applicationapi.model.Application;
+import com.jobapplicationapp.applicationapi.service.ApplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigInteger;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@RequestMapping("/api/")
+public class ApplicationController {
+    @Autowired
+    private ApplicationService applicationService;
+
+    @PostMapping("application")
+    public Application add(@RequestBody ApplicationRequest application) {
+        return this.applicationService.addApplication(application);
+    }
+
+    @GetMapping("application/{id}")
+    public Application getById(@PathVariable BigInteger id) {
+        return this.applicationService.find(id);
+    }
+
+
+    @GetMapping("application/all")
+    public List<Application> getAll(@PathVariable BigInteger id) {
+        return this.applicationService.getAll();
+    }
+}
